c7b8f7def3babe9b8e7fd9094076f026c72c7905
Asiabennett00 <asiabennett06@gmail.com>
2026-03-18 21:58:14 +0000

    added

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.multiple-items').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 1500
+    });
+
+
+});
